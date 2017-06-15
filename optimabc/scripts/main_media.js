6f6b3ff032cb5807a82a30763aa92b5abbb6a74c
macknoyer <yudin.nc@gmail.com>
2017-06-15 16:53:03 +0500

    commit2

--- optimabc/scripts/main_media.js
+++ optimabc/scripts/main_media.js
@@ -8,5 +8,22 @@ $(document).ready(function(){
 		$('.main_humb ul.menu').hide();
 		$('.humb_link').show()
 		$('.humb_link_close').hide();
+	});     
+	$("#owl-adaptive-slider1").owlCarousel({
+		      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      navigationText : false,
+      pagination:true
+	});	
+	$("#owl-adaptive-slider2").owlCarousel({
+		      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      navigationText : false,
+      pagination:true
 	});
+     
 });
