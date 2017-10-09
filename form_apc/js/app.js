ef89f80d5333013d59d17400fcfe8f6779e1c6f4
macknoyer <yudin.nc@gmail.com>
2017-10-09 19:35:46 +0500

    form_apc

--- form_apc/js/app.js
+++ form_apc/js/app.js
@@ -7,5 +7,16 @@
 			$(this).children('.search_results').toggleClass('active');
 			$(this).children('.search_box').toggleClass('active');
 		});
+		$('.add_fav_link').click(function(){
+			$(this).toggleClass('active');
+			if ($(this).hasClass('active')) {
+				$(this).children('.fa').removeClass('fa-star-o');
+				$(this).children('.fa').addClass('fa-star');
+			}
+			else {
+				$(this).children('.fa').addClass('fa-star-o');
+				$(this).children('.fa').removeClass('fa-star');
+			}
+		});
 	});
 })(jQuery);
