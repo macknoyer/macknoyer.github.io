2cf365523b315db24be51350608828b9b104c600
macknoyer <yudin.nc@gmail.com>
2017-10-26 02:06:34 +0500

    +

--- form_apc/js/app.js
+++ form_apc/js/app.js
@@ -36,5 +36,20 @@
 		$('.humb_link').click(function () {
     		$(this).parent().find('.header_menu').slideToggle();
 		});
+  var owl = $(".owl-carousel");
+ 
+  owl.owlCarousel({
+      items : 6, //10 items above 1000px browser width
+      itemsDesktop : [1000,3], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,2], // betweem 900px and 601px
+      itemsTablet: [600,1], //2 items between 600 and 0
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+ 
+  });
+ 
+  // Custom Navigation Events
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  })	
 	});
 })(jQuery);
