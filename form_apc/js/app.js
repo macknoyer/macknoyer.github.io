dcc9c60e78d6811d3e8de3f4ce89311520959e92
macknoyer <yudin.nc@gmail.com>
2017-11-07 14:35:32 +0500

    +

--- form_apc/js/app.js
+++ form_apc/js/app.js
@@ -18,15 +18,24 @@
 				$(this).children('.fa').removeClass('fa-star');
 			}
 		});
-		$('ul.colors li a').click (function (){
-			$(this).toggleClass('active'); 
-		if ($(this).hasClass('active')) {
-			$(this).parent().find('ul.colors li input[type="checkbox"]').attr('checked', true);
-		}
-		else {
-			$(this).parent().find('ul.colors li input[type="checkbox"]').attr('checked', false);
-		}
-		});
+        $('ul.colors li a').click (function (){
+            $(this).toggleClass('active'); 
+        if ($(this).hasClass('active')) {
+            $(this).parent().find('ul.colors li input[type="checkbox"]').attr('checked', true);
+        }
+        else {
+            $(this).parent().find('ul.colors li input[type="checkbox"]').attr('checked', false);
+        }
+        });
+        $('a.checkbox').click(function () {
+            $(this).toggleClass('active');
+            if ($(this).hasClass('active')) {
+                $(this).find('input[type="checkbox"]').attr('checked', true);
+            }
+            else {
+                $(this).find('input[type="checkbox"]').attr('checked', false);
+            }
+        });
 		$('.link_border').click(function(){
 			$(this).next('.review_popup').show();
 		});
